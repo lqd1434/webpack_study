bd6f8113ecc42b1424da022a9ddbd4507be926bb
lqd1434 <1434288209@qq.com>
2021-09-22 22:52:36 +0800

    :sparkles:更新配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,25 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const { ProgressPlugin } = require('webpack');
-const chalk = require('chalk');
+const {ESBuildPlugin} = require('esbuild-loader')
+const chalk = require('chalk')
+const ProgressBarPlugin = require('progress-bar-webpack-plugin')
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+const smp = new SpeedMeasurePlugin();
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
+const TerserPlugin = require('terser-webpack-plugin')
 
 
-
-const handler = (percentage, message, ...args) => {
-
-};
-
-module.exports = {
-	entry: {
-		"utils":"./src/utils.ts"
-	},
+module.exports = smp.wrap({
+	entry: "./lib/index.tsx",
 	output: {
-		filename: '[name].js',
-		library: 'Utils',
-		libraryTarget: "umd",
+		filename: 'bundle.js',
+		pathinfo: false,
+		// libraryTarget: "umd",
 		path: path.resolve(__dirname, 'dist'),
 	},
-	devtool: 'inline-source-map',
-	mode: 'production',
+	devtool: 'eval-cheap-module-source-map',
+	mode: 'development',
 	devServer: {
 		compress: true,
 		port: 9000,
@@ -29,36 +27,77 @@ module.exports = {
 			progress: true,
 		},
 	},
+	cache: {
+		type: 'filesystem', // 使用文件缓存
+	},
+	externals:{
+		"react":'React',
+		"react-dom":"ReactDOM"
+	},
 	plugins: [
-		new ProgressPlugin({
-			activeModules: false,
-			entries: true,
-			handler(percentage, message, ...args) {
-				handler(percentage, message, ...args)
-			},
-			modules: true,
-			modulesCount: 5000,
-			profile: false,
-			dependencies: true,
-			dependenciesCount: 10000,
-			percentBy: null,
+		new HtmlWebpackPlugin({template: "./lib/index.html"}),
+		new ESBuildPlugin(),
+			new BundleAnalyzerPlugin(),
+		new ProgressBarPlugin({
+			format: `  :msg [:bar] ${chalk.green.bold(':percent')} (:elapsed s)`
 		}),
-		// new HtmlWebpackPlugin({
-		// 	title: 'ReactApp',
-		// 	template: "./src/index.html"
-		// 		}),
-			new CleanWebpackPlugin(),
+		new CleanWebpackPlugin(),
 	],
 	module: {
 		rules: [
 			{
 				test: /\.tsx?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/
+				loader: 'esbuild-loader',
+				// exclude: /node_modules/,
+				include:path.resolve(__dirname, 'lib'),
+				options: {
+					loader:"tsx",
+					target:"es2015"
+				}
 			}
 		]
 	},
 	resolve: {
 		extensions: [ '.tsx', '.ts', '.js','jsx' ]
 	},
-};
+	optimization: {
+		minimizer: [
+			new TerserPlugin({
+				parallel: 4,
+				terserOptions: {
+					parse: {
+						ecma: 8,
+					},
+					compress: {
+						ecma: 5,
+						warnings: false,
+						comparisons: false,
+						inline: 2,
+					},
+					mangle: {
+						safari10: true,
+					},
+					output: {
+						ecma: 5,
+						comments: false,
+						ascii_only: true,
+					},
+				},
+			}),
+		],
+		splitChunks: {
+			// include all types of chunks
+			chunks: 'all',
+			// 重复打包问题
+			cacheGroups:{
+				vendors:{ // node_modules里的代码
+					test: /[\\/]node_modules[\\/]/,
+					chunks: "all",
+					// name: 'vendors', 一定不要定义固定的name
+					priority: 10, // 优先级
+					enforce: true
+				}
+			}
+		},
+	}
+});
